7d2617f5c4d6f0cabc7d748574c3d81be75cf8aa
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-02-28 15:26:27 +0000

    Added two files

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+//write aprogram to input users name and greet the user as output 
+#include<iostream>
+using namespace std;
+int main(){
+    string name;
+    cout<<"enter your name:";
+    cin>>name;
+    cout<<"hello, "<<name;
+    return 0;
+}
---
+++ 2.cpp
@@ -0,0 +1,10 @@
+//input nummbers and display sum and multiply
+#include<iostream>
+using namespace std;
+int main(){
+    int a, b;
+    cout<<"enter the two numbers:";
+    cin>>a>>b;
+    cout<<"sum: "<<a+b<<"/nMultiply:"<<a*b;
+    return 0;
+}
