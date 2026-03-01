f49a6b42bcb76f35f97d80fb4f9c6b6f0b04552b
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-01 15:24:52 +0000

    adding to repo

---
+++ 4.cpp
@@ -0,0 +1,11 @@
+//find the circumference and area of a circle
+#include<iostream>
+using namespace std;
+int main(){
+    float r;
+    cout<<"enter radius:";
+    cin>>r;
+    cout<<"Circumference:"<<2*3.14*r;
+    cout<<"  area:"<<3.14*r*r;
+    return 0;
+}
