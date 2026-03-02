bd3b775e99776dc3e0cf431f8c2208d06ac39bbc
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-02 17:36:18 +0000

    even odd prog

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+//check if input num is even or odd
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter any num:";
+    cin>>n;
+    if(n%2 == 0){
+        cout<<"num entered is even ";
+    }else{
+        cout<<"num entered is odd ";
+    }
+}
