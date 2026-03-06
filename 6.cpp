5747d445ba8e292f368b0f549a44c2dbcdfbef22
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-06 14:43:04 +0000

    Celsius to fehrenhiet

---
+++ 6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float c;
+    cout<<"enter your celsius value: ";
+    cin>>c;
+    float f = (9 * c)/5 + 32;
+    cout<<"the corresponding Fehrenhiet value is : "<<f;
+    return 0;
+    
+}
