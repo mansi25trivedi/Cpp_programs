06220eee76f68202eb2753ab328864306a155b77
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-03-24 13:11:42 +0000

    octal and hexadecimal representation

---
+++ 29.cpp
@@ -0,0 +1,10 @@
+//display numbers in octal and hexa decimal number system 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter any number: ";
+    cin>>n;
+    cout<<endl<<" octal value is : "<<oct<<n<<endl<<"hexadecimal value is: "<<hex<<n;
+    return 0;
+}
