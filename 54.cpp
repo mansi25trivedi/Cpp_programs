627de15f2db9e9672ceb0572f4ea177d42e37181
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-04-20 04:29:50 +0000

    traversal of string to each char is left

---
+++ 54.cpp
@@ -0,0 +1,11 @@
+//take a password string and check basic rules (lenght >=8 and atleast one digit)
+#include<iostream>
+using namespace std; 
+int main(){
+    string s;
+    cout<<"enter your password: ";
+    cin>>s;
+    int l = s.length();
+    cout<<"lenght : "<<l;
+    
+}
