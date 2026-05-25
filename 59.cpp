02f94ed73e13a96b360dd1509ed15bb3a945ee63
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-05-25 15:41:25 +0000

    dispay the century

---
+++ 59.cpp
@@ -0,0 +1,10 @@
+//Take a year and print the corresponding century (e.g., “19th century”, “20th century”) 
+#include<iostream>
+using namespace std;
+ int main(){
+    string year;
+    cout<<"enter any year :";
+    cin>>year;
+    cout<<"century is "<<year[0]<<year[1]<<endl;
+    return 0;
+ }
