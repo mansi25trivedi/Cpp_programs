5a3de2986897649ce37a80229fc6b4a1c11f278f
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-06-01 14:40:58 +0000

    squares print

---
+++ 66.cpp
@@ -0,0 +1,12 @@
+//Print the squares of numbers from 1 to n.
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter num form which you want the squares: ";
+    cin>>num;
+    for(int i = 1 ; i <= num ; i++){
+        cout<<i*i<<endl;
+    }
+    return 0;
+}
