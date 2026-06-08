aa1520c3e7fcf68a9abbff407c8e76c302c8c55b
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-06-08 12:51:03 +0000

    star pattern 2

--- 69.cpp
+++ 69.cpp
@@ -8,5 +8,13 @@ int main(){
     for(int i= 1; i<=n ; i++){
         cout<<"*";
     }
+    cout<<endl;
+    cout<<"Print Square of Stars (n x n Stars)"<<endl;
+    for(int k=1 ; k<=n ; k++){
+        for(int j =1 ; j<=n ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
