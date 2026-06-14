dd385c54b8ba68bba138c6636ff2cff3ed9d999a
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-06-14 07:28:53 +0000

    star pattern 7

--- 69.cpp
+++ 69.cpp
@@ -64,5 +64,16 @@ int main(){
         }
         cout<<endl;
     }
+
+    cout<<endl<<" Print Stars and Spaces Alternating (Stars and Blank Spaces)"<<endl;
+    for(int i = 0; i<n ;i++){
+        for(int j=n-1-i ; j>0 ;j--){
+            cout<<" ";
+        }
+        for(int k=0 ; k<=i ; k++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
