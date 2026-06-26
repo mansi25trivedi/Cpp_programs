f413e15059119e45e893ee1de8166c177e12e239
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-06-26 14:08:08 +0000

    diamond shape

--- 69.cpp
+++ 69.cpp
@@ -197,5 +197,39 @@ int main(){
         }
         cout<<endl;
     }
+
+    cout<<endl<<"diamond shape with stars"<<endl;
+    //staight triangle
+    for(int i = 0 ; i < n ; i++){
+        //foro space
+        for(int j = 0 ; j<(n-1)-i ;j++){
+            cout<<"  ";
+        }
+        //for stars
+        for(int j =0 ; j<=i ; j++){
+            cout<<"* ";
+        }
+        //for other half of the triangle
+        for(int j = i-1 ; j>=0 ; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    //inverted triangle
+    for(int i = n-1 ; i>=1 ;i--){
+        //for spaces
+        for(int j = (n-1)-i ; j>=1 ; j--){
+            cout<<"  ";
+        }
+        //for stars
+        for(int j= i ; j>=0 ; j--){
+            cout<<"* ";
+        }
+        //for other half of stars
+        for(int j=i-1 ; j>=0 ; j--){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
