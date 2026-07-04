12a749f0e7068bedf4c926d0d8eb1d5bd3f21057
Mansi Trivedi <trivedi.25mansi@gmail.com>
2026-07-04 16:59:20 +0000

    reverse num print

--- 72.cpp
+++ 72.cpp
@@ -10,11 +10,21 @@ void printnum(int n){
     cout<<n<<" ";
 
 }
+void printrenum(int n){
+    if(n==1){
+        cout<<1;
+        return;
+    }
+    cout<<n<<" ";
+    printrenum(n-1);
+}
 int main(){
     cout<<"Print numbers from 1 to n using recursion."<<endl;
     int n;
     cout<<"enter any number: ";
     cin>>n;
     printnum(n);
+    cout<<endl<<"Print numbers from n down to 1 using recursion."<<endl;
+    printrenum(n);
 
 }
